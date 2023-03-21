0757c432c4fad0fadfd5c89ca90424198df644b2
Konstantin Kostin <91910948+kvkostin@users.noreply.github.com>
2023-03-21 17:18:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,8 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 # Display the table on the page.
@@ -32,13 +30,10 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.write('The user entered ', fruit_choice)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
@@ -50,8 +45,8 @@ streamlit.dataframe(fruityvice_normalized)
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.dataframe(my_data_rows)
 
 another_fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding', another_fruit_choice) 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
