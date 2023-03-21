1f1ab9bc570967ab6377ce1867df66c9d3968cbb
Konstantin Kostin <91910948+kvkostin@users.noreply.github.com>
2023-03-21 17:33:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
-streamlit.write('The user entered ', fruit_choice)
+
 
 except URLError as e:
   streamlit.error()
